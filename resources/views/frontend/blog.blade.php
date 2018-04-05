3ad39677e59b72192290702adbf4cdd78ba815ea
jayanudin <jayanudin15@gmail.com>
2018-04-05 13:54:54 +0700

    create db:seed

--- resources/views/frontend/blog.blade.php
+++ resources/views/frontend/blog.blade.php
@@ -21,21 +21,27 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-8 col-md-10 mx-auto">
-    
-                @foreach($post as $key => $data)
-                    <div class="post-preview">
-                        <a href="{{ url('blog/detail/' . $data->slug) }}">
-                            <h2 class="post-title">{{ $data->title }}</h2>
-                            <h3 class="post-subtitle">
-                              {{ $data->category->title }}
-                            </h3>
-                        </a>
-                        <p class="post-meta">Posted by
-                        <a href="#">{{ $data->user->name }}</a> on {{ $data->created_at }}</p>
-                    </div>
 
-                    <hr>
-                @endforeach
+                @if($post)
+    
+                    @foreach($post as $key => $data)
+                        <div class="post-preview">
+                            <a href="{{ url('blog/detail/' . $data->slug) }}">
+                                <h2 class="post-title">{{ $data->title }}</h2>
+                                <h3 class="post-subtitle">
+                                  {{ $data->category->title }}
+                                </h3>
+                            </a>
+                            <p class="post-meta">Posted by
+                            <a href="#">{{ $data->user->name }}</a> on {{ $data->created_at }}</p>
+                        </div>
+
+                        <hr>
+                    @endforeach
+
+                @else
+                    <h3>No Post</h3>
+                @endif
 
                 <hr>
                 <!-- Pager -->
